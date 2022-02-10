b887e3a4413d759118f4ca9ac7facf15faee9371
zholloway <zholloway92@gmail.com>
2022-02-10 16:44:33 -0500

    html and js written

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const ToDoList = artifacts.require("ToDoList");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(ToDoList);
 };
